be26bc9315d8a3a37d6eae61cd99a919d794ad53
quentin7b <klein.quentin@gmail.com>
2015-10-24 20:07:12 +0200

    Layout looks better

--- public/js/controller_items.js
+++ public/js/controller_items.js
@@ -5,7 +5,19 @@ app.controller('ItemCtrl', function ($scope, ItemService) {
     ItemService
         .getItems()
         .then(function(data){
-            $scope.deprecatedItems = data;
+            var sortedData = {};
+            var dataCount = data.length;
+            for(var index = 0; index < dataCount; index++) {
+                var deprecatedItem = data[index];
+                var deprecatedGroup = deprecatedItem.deprecated_item.group + '';
+                if(sortedData[deprecatedGroup] == undefined) {
+                    sortedData[deprecatedGroup] = [];
+                }
+                var itemsArray = sortedData[deprecatedGroup];
+                itemsArray.push(deprecatedItem);
+                sortedData[deprecatedGroup] = itemsArray;
+            }
+            $scope.deprecatedItems = sortedData;
         }, function(err) {
              $scope.err = err;
         });
