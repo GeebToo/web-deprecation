2abdecefe0186ba0132bcbaefca658705dd1ecad
quentin7b <klein.quentin@gmail.com>
2015-10-25 15:49:39 +0100

    Better sort and hover color

--- public/js/controller_items.js
+++ public/js/controller_items.js
@@ -1,24 +1,11 @@
 app.controller('ItemCtrl', function ($scope, ItemService) {
     // Initialize values to make loading fluid
+    $scope.sort = 'rootSort';
     $scope.data = {};
     $scope.deprecatedItems = {};
     // Declare functions
     $scope.sortByRoot = function() {
-        var sortedData = {};
-        var dataCount = $scope.data.length;
-        for(var index = 0; index < dataCount; index++) {
-            var deprecatedItem = $scope.data[index];
-            var deprecatedGroup = deprecatedItem.belongs_to + '';
-            if(sortedData[deprecatedGroup] == undefined) {
-                sortedData[deprecatedGroup] = [];
-            }
-            var itemsArray = sortedData[deprecatedGroup];
-            itemsArray.push(deprecatedItem);
-            sortedData[deprecatedGroup] = itemsArray;
-        }
-        $scope.deprecatedItems = sortedData;
-    };
-    $scope.sortByApps = function() {
+        $scope.sort = 'rootSort';
         var sortedData = {};
         var dataCount = $scope.data.length;
         for(var index = 0; index < dataCount; index++) {
@@ -33,7 +20,24 @@ app.controller('ItemCtrl', function ($scope, ItemService) {
         }
         $scope.deprecatedItems = sortedData;
     };
+    $scope.sortByApps = function() {
+        $scope.sort = 'appsSort';
+        var sortedData = {};
+        var dataCount = $scope.data.length;
+        for(var index = 0; index < dataCount; index++) {
+            var deprecatedItem = $scope.data[index];
+            var deprecatedGroup = deprecatedItem.belongs_to + '';
+            if(sortedData[deprecatedGroup] == undefined) {
+                sortedData[deprecatedGroup] = [];
+            }
+            var itemsArray = sortedData[deprecatedGroup];
+            itemsArray.push(deprecatedItem);
+            sortedData[deprecatedGroup] = itemsArray;
+        }
+        $scope.deprecatedItems = sortedData;
+    };
     $scope.sortByDate = function() {
+        $scope.sort = 'dateSort';
         alert('date');
     }
     // Load data
