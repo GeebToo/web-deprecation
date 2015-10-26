de695fa05d4d1783ac21690d249d6775ff42ddab
quentin7b <klein.quentin@gmail.com>
2015-10-26 19:11:41 +0100

    Fixes date issue

--- public/js/controller_create.js
+++ public/js/controller_create.js
@@ -1,11 +1,17 @@
-app.controller('CreateCtrl', function ($scope, ItemService) {
+app.config(function($mdDateLocaleProvider) {
+    $mdDateLocaleProvider.formatDate = function(date) {
+        return moment(date).format('YYYY/MM/DD');
+    };
+}).controller('CreateCtrl', function ($scope, ItemService) {
     // Initialize values to make loading fluid
     $scope.belong_items = ['IOS', 'ANDROID', 'WEB'];
     $scope.item = {
-        deprecation_date: moment().format(),
-        unusable_date: moment().add(6, 'month').format()
+        deprecation_date: new Date(moment().format()),
+        unusable_date: new Date(moment().add(6, 'month').format())
     };
     $scope.saveItem = function(){
+        $scope.item.deprecation_date = moment($scope.item.deprecation_date).format(); // Convert dates
+        $scope.item.unusable_date = moment($scope.item.unusable_date).format();
         console.log($scope.item);
     };
 });
